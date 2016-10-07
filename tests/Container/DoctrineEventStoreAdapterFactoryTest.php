2f8df29b0128bcdb1f0afce875cbaa9e8ad26d0d
prolic <saschaprolic@googlemail.com>
2016-10-08 05:36:19 +0800

    update copyright, composer and travis

--- tests/Container/DoctrineEventStoreAdapterFactoryTest.php
+++ tests/Container/DoctrineEventStoreAdapterFactoryTest.php
@@ -1,12 +1,11 @@
 <?php
-/*
+/**
  * This file is part of the prooph/event-store-doctrine-adapter.
- * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * Date: 8/24/15 - 12:09 AM
  */
 namespace ProophTest\EventStore\Adapter\Doctrine\Container;
 
