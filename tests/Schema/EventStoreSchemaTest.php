53d285bb5599c091f194a5aa49929d4f60971a15
prolic <saschaprolic@googlemail.com>
2016-10-08 05:37:59 +0800

    declare(strict_types=1);

--- tests/Schema/EventStoreSchemaTest.php
+++ tests/Schema/EventStoreSchemaTest.php
@@ -7,6 +7,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
+
 namespace ProophTest\EventStore\Adapter\Doctrine\Schema;
 
 use Doctrine\DBAL\Schema\Schema;
